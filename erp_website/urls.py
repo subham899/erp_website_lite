4339cfeb1f55d9d8b6a6c508dd24c6c2a191cb4f
subham899 <67275085+subham899@users.noreply.github.com>
2020-07-10 18:08:12 +0530

    dbg

--- erp_website/urls.py
+++ erp_website/urls.py
@@ -20,6 +20,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('websiteapp.urls')),
-    path('', include('eployee_app.urls')),
+    #path('', include('eployee_app.urls')),
 
 ]
